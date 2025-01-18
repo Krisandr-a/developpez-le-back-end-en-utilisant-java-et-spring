75f1126e5bf05a24961edd598f9825b3ad03b6dd
krisandra <krisandra.adams@gmail.com>
2025-01-18 17:18:50 +0100

    feat: added mysql configuration and initial database sql

--- src/main/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication.java
+++ src/main/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication.java
@@ -1,13 +1,18 @@
 package com.example.developpez_le_back_end_en_utilisant_java_et_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication {
+public class DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
