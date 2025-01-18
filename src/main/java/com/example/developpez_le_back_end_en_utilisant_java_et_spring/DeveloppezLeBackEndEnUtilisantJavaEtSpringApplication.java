0a2a784f3390b6ac2d3c0b43ffea3dcd27a1a27d
krisandra <krisandra.adams@gmail.com>
2025-01-18 11:47:45 +0100

    feat: first commit

---
+++ src/main/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.developpez_le_back_end_en_utilisant_java_et_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloppezLeBackEndEnUtilisantJavaEtSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/DeveloppezLeBackEndEnUtilisantJavaEtSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.developpez_le_back_end_en_utilisant_java_et_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloppezLeBackEndEnUtilisantJavaEtSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
