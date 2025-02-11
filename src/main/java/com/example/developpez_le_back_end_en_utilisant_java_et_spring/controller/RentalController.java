dd4940431c33ef3a8c74cc25ae4ef6d3e3c2400f
krisandra <krisandra.adams@gmail.com>
2025-02-11 21:40:13 +0100

    fix: corrected response for adding a rental

--- src/main/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/controller/RentalController.java
+++ src/main/java/com/example/developpez_le_back_end_en_utilisant_java_et_spring/controller/RentalController.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rentals")
@@ -35,8 +37,9 @@ public class RentalController {
 
     @PostMapping
     @Operation(summary = "Ajouter une nouvelle location")
-    public RentalDto createRental(@RequestBody AddRentalDto rentalRequest) {
-        return rentalService.addRental(rentalRequest);
+    public ResponseEntity<Map<String, String>> createRental(@RequestBody AddRentalDto rentalRequest) {
+        rentalService.addRental(rentalRequest);
+        return ResponseEntity.ok(Map.of("message", "Rental created!"));
     }
 
     @PutMapping("/{id}")
